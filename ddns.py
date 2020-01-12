9d1acdf6fd45ed6b07f7a9e77951017b1629a498
Danilo Clemente <danilo.clemente@icloud.com>
2020-01-12 15:39:01 -0300

    correct loop to include all rules found

--- ddns.py
+++ ddns.py
@@ -1,10 +1,5 @@
-'''
-Dynamic DNS for Firewall for Vultr
-By MBRCTV
-
-credit for https://github.com/andyjsmith/Vultr-Dynamic-DNS
-'''
 import requests
+import time
 import json
 import socket
 
@@ -27,11 +22,12 @@ else:
 
 for list_rules in firewallgroupids:
     firewallgroup = list_rules
-    print(list_rules)
-    
+    #print(list_rules)
+
     # Get the list of Firewall records from Vultr to update the ones with user setup in config file
-    raw_rules = json.loads(requests.get("https://api.vultr.com/v1/firewall/rule_list?FIREWALLGROUPID=" +
-                                        firewallgroup + "&direction=in&ip_type=v4", headers={"API-Key": api_key}).text)                                
+    time.sleep(1) # 1 second waiting before making requests, to avoid reach Vultr rate hits
+    url = "https://api.vultr.com/v1/firewall/rule_list?FIREWALLGROUPID=" + firewallgroup + "&direction=in&ip_type=v4"
+    raw_rules = requests.get(url, headers={"API-Key": api_key}).json()
 
     # Make a new varible with the vultr ip
     v_ip = ""
@@ -44,47 +40,53 @@ for list_rules in firewallgroupids:
             v_subnet_size = raw_rules[rule]["subnet_size"]
             v_subnet = raw_rules[rule]["subnet"]
             v_ip = v_subnet
+            #print(v_ip + ":" + v_port)
+            needsUpdated = False
+            if firewallgroup + v_ip + ":" + v_port != firewallgroup + ip + ":" + v_port:
+                needsUpdated = True
+            #print(needsUpdated)
 
-    # Cancel if no firewall records from Vultr match with user parameter in the config file, there's no rule to update
-    if len(v_ip) == 0:
-        print("Configuration error, no ip found for this user.")
-        quit()
+            if len(v_ip) == 0:
+                print("Configuration error, no ip found for this user.")
+                quit()
 
-    # Check if the IP address actually differs from any of the records
-    needsUpdated = False
-    if v_ip != ip:
-        needsUpdated = True
+            # Cancel if the IP has not changed
+            if not needsUpdated:
+                print("For firewall group rule " + list_rules 
+                      + " your IP:Port is: " + ip +":"+ v_port 
+                      + ". Rule has not changed and have not been updated.")
+            else:
+                for rule in raw_rules:
+                    print("Your IP for the rule " + list_rules + " has changed since last checking"
+                          + ". Old IP on Vultr: " + v_ip + ":" + v_port
+                          + " Current Device IP: " + ip + ":" + v_port)
 
-    # Cancel if the IP has not changed
-    if not needsUpdated:
-        print("your ip is: " + ip +
-            " \nIP address has not changed. No rules have been updated.")
-    else:
-        print("your IP has changed since last checking.")
-        print("Old IP on Vultr: " + v_ip + ", current Device IP: " + ip)
+                    # Remove old Firewall rule
+                    time.sleep(1) # 1 second waiting before making requests, to avoid reach Vultr rate hits
+                    payload = {"FIREWALLGROUPID": list_rules, "rulenumber": v_rulenumber}
+                    response = requests.post("https://api.vultr.com/v1/firewall/rule_delete",
+                                            data=payload, headers={"API-Key": api_key})
+                    if response.status_code == 200:
+                        print("Current rule for " + v_ip + ":" + v_port 
+                              + " for group id " + list_rules + " has been deleted")
+                    else:
+                        print("Deleting port:" + v_port 
+                              + " in rule id: " + firewallgroup)
 
-        # Remove old Firewall rule
-        payload = {"FIREWALLGROUPID": firewallgroup, "rulenumber": v_rulenumber}
-        response = requests.post("https://api.vultr.com/v1/firewall/rule_delete",
-                                data=payload, headers={"API-Key": api_key})
-        if response.status_code == 200:
-            print("Current rule for " + user + " has been deleted")
-        else:
-            print("Error deleting rule")
-
-        # Update the rule in Vultr with the new IP address
-        payload = {"FIREWALLGROUPID": firewallgroup,
-                "direction": "in",
-                                "ip_type": "v4",
-                                "protocol": v_protocol,
-                                "subnet": ip,
-                                "subnet_size": v_subnet_size,
-                                "port": v_port,
-                                "notes": v_notes}
-        response = requests.post("https://api.vultr.com/v1/firewall/rule_create",
-                                data=payload, headers={"API-Key": api_key})
-        if response.status_code == 200:
-            print("user " + user + " has been updated to " + ip)
-        else:
-            print("Error adding rule")  
-quit()
+                    # Update the rule in Vultr with the new IP address
+                    time.sleep(1) # 1 second waiting before making requests, to avoid reach Vultr rate hits
+                    payload = {"FIREWALLGROUPID": firewallgroup,
+                            "direction": "in",
+                                            "ip_type": "v4",
+                                            "protocol": v_protocol,
+                                            "subnet": ip,
+                                            "subnet_size": v_subnet_size,
+                                            "port": v_port,
+                                            "notes": v_notes}
+                    response = requests.post("https://api.vultr.com/v1/firewall/rule_create",
+                                            data=payload, headers={"API-Key": api_key})
+                    if response.status_code == 200:
+                        print("user " + user + " has been updated to " + ip + ":" + v_port)
+                        break
+                    else:
+                        print("Error adding rule")
